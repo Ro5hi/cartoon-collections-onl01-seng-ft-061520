173a93f977d8e102be2c69a4be4fb4d7eb58e9ad
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-20 16:07:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+
+  dwarves.each_with_index do |names, index|
+  puts "#{1+index}. #{names}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.each do |veg|
+    puts "#{veg.capitalize}!"
+   end
 end
 
 def long_planeteer_calls# code an argument here
