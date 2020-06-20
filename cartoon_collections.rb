c9c1c618a75372350979001a1e2ae9af2ec7aa1c
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-20 18:32:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,17 +6,36 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.each do |veg|
-    puts "#{veg.capitalize}!"
-   end
-    fruits.each do |fruit|
-    puts "#{fruit.capitalize}!"
+  veggies.collect do |veg|
+    "#{veg.capitalize}!"
+  end 
+end 
+
+
+
+def long_planeteer_calls(calls)
+  calls.any? do |c|
+    c.length > 4
   end
 end
 
-def long_planeteer_calls
-end
-
 def find_the_cheese(cheddar_cheese)
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |cheddar|
+  if cheese_types.include? "cheddar"
+  result = cheddar
+  return cheddar
+    end
+  end
+end 
+
+def find_the_cheese(no_cheese)
+  no_cheese = [ham cellphone computer]
+  no_cheese.each do |cheese|
+  if no_cheese.include? "cheese"
+  return results
+    end 
+  end 
 end
